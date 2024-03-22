39079084659a99484cb94aa6ab0d770d64c9bb4b
geisalopes <72300375+geisalopes@users.noreply.github.com>
2024-03-22 16:29:48 +0100

    Add axios, and api to generate garden advices

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-function generateAdvisor(event) {
-  event.preventDefault();
-
+function displayAdvice(response) {
   new Typewriter("#advice", {
-    strings: "Pothos are easy to take care",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
 
+function generateAdvisor(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "bf12f0ob06f7acf048dt44a41aadd939";
+  let context =
+    "You're a plant expert and love writing short advices. Your mission is to generate a short piece of advice, in basic HTML. Don't include a title in the advice. Sign the advice with 'Your Garden Advisor' inside a <strong/> element, only at the end of the advice. Make sure you follow the user instructions";
+  let prompt = `Generate an advice on ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let adviceElement = document.querySelector("#advice");
+  adviceElement.innerHTML = `<div class="generating">⏳ Generating a advice for you.</div>`;
+
+  axios.get(apiUrl).then(displayAdvice);
+}
+
 let advisorElement = document.querySelector("#advice-generator-form");
 advisorElement.addEventListener("submit", generateAdvisor);
-
-// api key
-// bf12f0ob06f7acf048dt44a41aadd939
